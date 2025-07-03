d1ae1e44cd0d98e75746a90bc4d6c03f011c9668
William <you@example.com>
2025-07-02 21:39:18 -0300

    feat: alterando head

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -19,21 +19,26 @@ export function Hero() {
 			</div>
 			<div className=" flex justify-start items-center gap-4 bg-[#6c8c7d] px-4 py-4 md:gap-10 md:px-16 md:py-6 opacity-95">
 				<p className="font-bold text-md underline underline-offset-8 ">
-					Dra. Isabel Meireles
+					Dra. Emilaine Lopes
 				</p>
-				<p className="md:text-sm text-[10px] ml-6">Psicóloga Clínica</p>
-				<p className="md:text-sm text-[10px]">@dra.isameireles</p>
+				<p className="md:text-sm text-[10px] ml-6 italic">Psicóloga Clínica</p>
+				<p className="md:text-sm text-[10px] italic">@elapsico</p>
 			</div>
 
 			<div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
 				<article className="grid grid-cols-1 lg:grid-cols-2 gap-8">
 					<div className="space-y-6">
-						<h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10 text-[#4b4b4b]">
-							Transforme sua saúde emocional com acolhimento e empatia
+						<h1 className="text-3xl md:text-2xl lg:text-3xl font-semibold leading-6 text-[#4b4b4b] tracking-tighter">
+							A vida é feita de alegrias e tristezas mas, fazer{" "}
+							<span className="md:text-4xl font-bold italic">PSICOTERAPIA</span>{" "}
+							é como ter um{" "}
+							<span className="md:text-4xl font-bold italic">LAR</span> para
+							onde voltar de forma simbólica, onde eu seguro a sua mão e você
+							pode simplesmente{" "}
+							<span className="md:text-4xl font-bold italic">SER!</span>
 						</h1>
-						<p className="lg:text-lg text-[#4b4b4b] font-serif">
-							Psicoterapia individual para adultos e adolescentes, com foco no
-							seu bem-estar e autoconhecimento.
+						<p className="lg:text-lg text-[#4b4b4b] italic">
+							Psicoterapia individual, presencial e online.
 						</p>
 						<div>
 							<Button
